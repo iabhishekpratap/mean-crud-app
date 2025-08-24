be2da8fa6b6f8b78e9fb8515e523cadf031ecbc1
iabhishekpratap <iapsingh@outlook.com>
2025-08-24 14:29:06 +0000

    modified:   frontend/src/app/services/tutorial.service.ts

--- frontend/src/app/services/tutorial.service.ts
+++ frontend/src/app/services/tutorial.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
-const baseUrl = 'http://localhost:8080/api/tutorials';
+const baseUrl = 'http://3.7.251.230:8080/api/tutorials';
 
 @Injectable({
   providedIn: 'root'
